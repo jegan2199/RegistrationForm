e5f1a1add37e9c3af735b5acd97e2387f64da169
jegan2199 <70738352+jegan2199@users.noreply.github.com>
2024-05-22 22:47:13 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
+import DashboardPage from "./pages/Dashboard";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/register" element={<RegisterPage/>} />
+                <Route path="/login" element={<LoginPage/>} />
+                <Route path="/dashboard" element={<DashboardPage/>} />
+                <Route path="/" element={<h1>Welcome Home</h1>} />
+            </Routes>
+        
+        
+        </BrowserRouter>
+
+        
     </div>
   );
 }
